8810ddcc32a36d0660d31d5dfa6e2584c3d00fb4
erpadmin <erpadmin@Frappe>
2021-09-14 15:34:32 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Compliance
+
+Compliance
+
+#### License
+
+MIT
---
+++ compliance/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/compliance"
+# docs_base_url = "https://[org_name].github.io/compliance"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Compliance"
